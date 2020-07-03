d0f31d1e218b2b7793ac40fd6de6395f8077adc5
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-02 23:19:23 -0300

    Arrays

---
+++ problema-matrizes/src/applications/Matrizes.java
@@ -0,0 +1,32 @@
+package applications;
+
+import java.util.Scanner;
+
+public class Matrizes {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[][] mat = new int[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		int x = sc.nextInt();
+		
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
+				if (mat[i][j] == x) {
+					System.out.println("Position " + i + "," + j + ":");
+				}
+			}
+		}
+		sc.close();
+	}
+
+}
