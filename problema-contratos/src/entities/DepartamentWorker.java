c711f3ffa3b65cd5a21998ab142f3067ea30621f
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-03 15:54:20 -0300

    Enumerações

---
+++ problema-contratos/src/entities/DepartamentWorker.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class DepartamentWorker {
+	private String name;
+	
+	public DepartamentWorker() {
+		
+	}
+
+	public DepartamentWorker(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
