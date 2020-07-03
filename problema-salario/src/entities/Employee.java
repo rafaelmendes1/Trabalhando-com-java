6ca2cc7c283d5e2b01015e416a0bd2c98d590ce4
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-03 15:36:13 -0300

    Introdução a POO

---
+++ problema-salario/src/entities/Employee.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Employee {
+		public String name;
+		public double grossSalary, tax;
+		
+		public double netSalary() {
+			return grossSalary - tax;
+		}
+		public void  increaseSalary(double percentage) {
+			grossSalary += grossSalary * (percentage/100);
+		}
+		public String toString() {
+			return name
+					+(", $")
+					+netSalary();
+		}
+		
+		
+}
