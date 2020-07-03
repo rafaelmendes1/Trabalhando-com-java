9046e0b5904e1a84794164c500b5cddcc593f2b5
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-03 16:41:12 -0300

    JDBC

---
+++ jdbc3/src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnction();
+			
+			st = conn.prepareStatement("insert into seller " 
+			+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+			+ "VALUES "
+			+"(?,?,?,?,?)",
+			Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, "Zoro");
+			st.setString(2, "melhorespadachim@domundo.saque");
+			st.setDate(3, new java.sql.Date(sdf.parse("11/11/1999").getTime()));
+			st.setDouble(4, 320000000);
+			st.setInt(5, 1);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Rows Affected: " + rowsAffected);
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(ParseException e) {
+			e.printStackTrace();
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+	}
+
+}
